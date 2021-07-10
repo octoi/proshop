08bb9b749de00f2cca87b8ffb4e31615d84fdfe0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-10 17:17:33 +0530

    added backend

---
+++ backend/base/urls/order_urls.py
@@ -0,0 +1,13 @@
+from django.urls.resolvers import URLPattern
+from django.urls import path
+from base.views import order_views as views
+
+urlpatterns = [
+    path('', views.getOrders, name='all-orders'),
+    path('add/', views.addOrderItems, name='orders-add'),
+    path('myorders/', views.getMyOrders, name='my-orders'),
+
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='deliver'),
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='paid'),
+]
---
+++ backend/base/views/user_views.py
@@ -0,0 +1,113 @@
+from django.contrib.auth.models import User, update_last_login
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.serializers import ProductSerializer, UserSerializer, UserSerializersWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializersWithToken(self.user).data
+
+        for key, value in serializer.items():
+            data[key] = value
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializersWithToken(user, many=False)
+        return Response(serializer.data)
+
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializersWithToken(user, many=False)
+
+    data = request.data
+    user.first_name = data['name']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+
+    data = request.data
+    user.first_name = data['name']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+
+    user.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion.delete()
+    return Response('User was deleted')
